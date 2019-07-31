829cc3f5cce6f73ae6e4938665903ea999aeb3d0
faraznaeem <faraznaeem@gmail.com>
2019-07-31 19:28:00 +0200

    Adds function for incorrect entry

--- app.js
+++ app.js
@@ -6,11 +6,22 @@ let min = 1,
 const game = document.querySelector('#game'),
       minNum = document.querySelector('.min-num'),
       maxNum = document.querySelector('.max-num'),
-      guessBtn = document.querySelector('#guess.-btn'),
+      guessBtn = document.querySelector('#guess-btn'),
       guessInput = document.querySelector('#guess-input'),
       message = document.querySelector('.message');
 
 minNum.textContent = min;
 maxNum.textContent = max;
 
+guessBtn.addEventListener('click', function(){
+    let guess = parseInt(guessInput.value);
+    console.log(guess)
 
+    if (isNaN(guess) || guess < min || guess > max){
+        setMessage(`Please enter a number between ${min} and ${max}`);
+    }
+});
+
+function setMessage(msg){
+    message.textContent = msg
+}
